9a63e98be03688a174cd91ef2a2773d0bd21c913
harshadadivekar-spec <harshadadivekar799@gmail.com>
2026-06-30 13:06:26 +0530

    add

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
+// app.get('/', (req, res) => {
+//     // res.send('Hello Express!');
+//     res.sendFile(__dirname+"/index.html")
+// });
+
+// app.listen(3000, () => {
+//     console.log('Server running on port 3000');
+// });
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home route
 app.get('/', (req, res) => {
-    // res.send('Hello Express!');
-    res.sendFile(__dirname+"/index.html")
+    res.send(`
+        <h1>Welcome to My Node.js Application</h1>
+        <p>This application is running successfully.</p>
+        <a href="/about">About Us</a>
+    `);
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+// About route
+app.get('/about', (req, res) => {
+    res.send(`
+        <h2>About Page</h2>
+        <p>This is a sample Express application.</p>
+        <a href="/">Go Back</a>
+    `);
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
